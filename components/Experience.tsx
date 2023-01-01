1491c1cccc4a25b18c323641adbb76ead2a43d06
nathan <thenathannicholson@gmail.com>
2023-01-01 14:16:54 -0800

    add the leva gui and add a setting to freeze the sphere

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,17 +1,27 @@
 import { OrbitControls } from "@react-three/drei"
-import { Ref, useRef } from "react"
+import { useRef } from "react"
 import Sphere from "./Sphere"
 import Floor from "./Floor"
 import { useFrame } from "@react-three/fiber"
+import { useControls } from "leva"
 
 const Experience = () => {
   const sphereRef = useRef<any>()
 
+  const { freezeSphere } = useControls({
+    freezeSphere: {
+      value: false,
+      label: "Freeze Sphere",
+    },
+  })
+
   useFrame(({ clock, camera }) => {
     const elapsedTime = clock.getElapsedTime()
 
-    sphereRef.current.position.y = Math.cos(elapsedTime * 2) / 5
-    sphereRef.current.position.y = Math.sin(elapsedTime * 2) / 5
+    if (!freezeSphere) {
+      sphereRef.current.position.y = Math.cos(elapsedTime * 2) / 5
+      sphereRef.current.position.y = Math.sin(elapsedTime * 2) / 5
+    }
   })
 
   return (
